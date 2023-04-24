54853ee862858feb5ca5b6577cd8891f1de08ed3
Siarhei Yafimenka <h1_n1@mail.ru>
2023-04-24 20:08:31 +0300

    feat: add class Active on button click

--- virtual-keyboard/src/index.js
+++ virtual-keyboard/src/index.js
@@ -33,23 +33,31 @@ wrapContent.insertAdjacentHTML('beforeend', html);
 
 // create buttons
 const language = 'en';
+const buttons = {
+  en: [
+    ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
+    ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'],
+    ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter'],
+    ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'Shift'],
+    ['Ctrl', 'Win', 'Alt', ' ', 'Alt', '◄', '▼', '►', 'Ctrl'],
+  ],
+  ru: [
+    ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
+    ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\'],
+    ['CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter'],
+    ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '▲', 'Shift'],
+    ['Ctrl', 'Win', 'Alt', ' ', 'Alt', '◄', '▼', '►', 'Ctrl'],
+  ],
+  id: [
+    ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'],
+    ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash'],
+    ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter'],
+    ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight'],
+    ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'],
+  ],
+};
+
 function createButtons() {
-  const buttons = {
-    en: [
-      ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
-      ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'],
-      ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter'],
-      ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', 'Shift'],
-      ['Ctrl', 'Win', 'Alt', ' ', 'Alt', '◄', '▼', '►', 'Ctrl'],
-    ],
-    ru: [
-      ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
-      ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\'],
-      ['CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter'],
-      ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '▲', 'Shift'],
-      ['Ctrl', 'Win', 'Alt', ' ', 'Alt', '◄', '▼', '►', 'Ctrl'],
-    ],
-  };
   const arr = buttons[language];
 
   arr.forEach((el, index) => {
@@ -57,16 +65,33 @@ function createButtons() {
     elRow.classList.add(`row${index + 1}`);
     elRow.classList.add('keyboard__row');
 
-    el.forEach((key) => {
+    el.forEach((key, keyIndex) => {
       const keyElement = document.createElement('button');
       keyElement.innerHTML = `${key}`;
+      keyElement.id = `${buttons.id[index][keyIndex]}`;
       keyElement.setAttribute('type', 'button');
       keyElement.classList.add('keyboard__button');
       elRow.append(keyElement);
     });
     keyboard.append(elRow);
   });
-
-  console.log(arr);
 }
 createButtons();
+
+// добавить класс Active при нажатии на кнопку настоящей клавиатуры
+const button = document.getElementsByClassName('keyboard__button');
+// console.log(button);
+body.addEventListener('keydown', (event) => {
+  for (let i = 0; i < button.length; i++) {
+    if (button[i].id == event.code) {
+      button[i].classList.add('active');
+    }
+  }
+});
+body.addEventListener('keyup', (event) => {
+  for (let i = 0; i < button.length; i++) {
+    if (button[i].id == event.code) {
+      button[i].classList.remove('active');
+    }
+  }
+});
